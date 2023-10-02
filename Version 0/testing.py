87f15e94c9e0a4b7226beba3cfd2c2f34c3b8c33
Nemis234 <103248645+Nemis234@users.noreply.github.com>
2023-10-02 11:27:37 +0200

    Delete Version 0 directory, deprecated

--- Version 0/testing.py
+++
@@ -1,4 +0,0 @@
-def hei(morn:tuple=(str:="1",int:=2)):
-    print(morn,type(morn))
-
-hei()
--- Version 0/testingdamn.py
+++
@@ -1,5 +0,0 @@
-string1 = "!label"
-string2 = "!frame"
-
-if any([x in string1 for x in ["!label","!frame"]]):
-    print(string1)
